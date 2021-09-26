90a7f869a35026df3965eab9e16873147682c852
Park <rppr001@naver.com>
2021-09-26 17:45:52 +0900

    react  material ui v4 and v5 error commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,36 @@
 import Customer from './components/Customer';
 import './App.css';
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import TableHead from '@mui/material/TableHead';
+import TableBody from '@mui/material/TableBody';
+import TableRow from '@mui/material/TableRow';
+import TableCell from '@mui/material/TableCell';
+import {createTheme, makeStyles, withStyles} from "@mui/material/styles";
+import withWidth from "@mui/material/Hidden/withWidth"; //css 적용하기
+
+const theme = createTheme();
+
+const styles = makeStyles((theme) =>{
+    root:{
+        width: '100%',
+
+    }
+});
+
+
+
+const styles = theme => ({
+    root:{
+        width: '100%',
+        marginTop:theme.spacing.unit * 3,
+        overflowX:"auto"
+    },
+    table:{
+        minWidth: 1080
+    }
+})
+
 
 const customers =
     [{
@@ -29,22 +60,32 @@ const customers =
     ]
 
 function App() { // 웹사이트 화면 출력
-  return (
-        <div>
-            {
-                customers.map(a => {
-                    return (<Customer
-                        key={a.id}
-                        id={a.id}
-                        image={a.image}
-                        name={a.name}
-                        birthday={a.birthday}
-                        job={a.job}
-                    />)
-                })
-            }
-    </div>
+    const { classes } = this.props;
+    return (
+        <Paper className={classes.root}>
+            <Table className={ classes.table}>
+                <TableHead>
+                    <TableCell>번호</TableCell>
+                    <TableCell>이미지</TableCell>
+                    <TableCell>이름</TableCell>
+                    <TableCell>생년월일</TableCell>
+                    <TableCell>성별</TableCell>
+                    <TableCell>직업</TableCell>
+                </TableHead>
+                <TableBody>
+                    {customers.map( a => { return (<Customer
+                      key={a.id}
+                      id={a.id}
+                       image={a.image}
+                       name={a.name}
+                       birthday={a.birthday}
+                      gender={a.gender}
+                       job={a.job}
+                       />)})}
+                   </TableBody>
+            </Table>
+    </Paper>
   );
 }
 
-export default App;
+export default withStyles(styles(App));
