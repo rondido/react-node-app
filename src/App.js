a4a07c0f527f76bcc6130eda4811ba08e44030fd
Park <rppr001@naver.com>
2021-09-26 15:31:23 +0900

    react node app project props commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import logo from './logo.svg';
+import Customer from './components/Customer';
 import './App.css';
+
+const customer ={
+    'name':'박짜장',
+    'birthday' : '941118',
+    'gender' : '남자',
+    'job' : '대학생'
+}
 
 function App() { // 웹사이트 화면 출력
   return (
-    <div className="gray-background">
-      <img src={logo} lat="logo"/>
-        <h2>Let`s develop project</h2>
-    </div>
+      <Customer
+        name={customer.name}
+        birthday={customer.birthday}
+        gender={customer.gender}
+        job={customer.job}
+      />
   );
 }
 
